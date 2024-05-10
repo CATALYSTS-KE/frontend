8ea8dfac17a5ca37ddd35d071d8a120013b0500f
nashthecoder <naijeria@gmail.com>
2024-05-10 18:50:35 +0300

    updated footer font size

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="footer bg-[url('@/assets/images/footer_bg.svg')]  text-white flex flex-col items-center w-full h-full">
+  <div class="footer bg-[url('@/assets/images/footer_bg.svg')]  text-white flex flex-col items-center w-full">
     <!-- link to the pages container -->
-    <div class="grid md:grid-cols-2 lg:grid-cols-4">
+    <div class="grid md:grid-cols-2 lg:grid-cols-4 text-2xl">
       <div class="mb-6 mx-20">
         <h1>ABOUT US</h1>
         <div class="flex flex-col">
-          <RouterLink to="/">Who We Are</RouterLink>
+          <RouterLink to="/">Who we are</RouterLink>
           <RouterLink to="/"> What We Do</RouterLink>
         </div>
       </div>
       <div class="mb-6">
         <h1>UPDATES</h1>
-        <div class="flex flex-col ">
+        <div class="flex flex-col">
           <RouterLink to="/">Stories</RouterLink>
           <RouterLink to="/">News/Articles</RouterLink>
           <RouterLink to="/">Events</RouterLink>
@@ -35,11 +35,11 @@
       </div>
     </div>
     <!-- contact info -->
-    <div class="pt-8">
+    <div class="pt-8 text-2xl">
       <span class="text-yellow-600">CONTACTS:</span><span> www.catalystsafrica.org</span></div>
     <!-- socials -->
-    <div class="p-12 flex justify-center ">
-      <div class="flex gap-4 pb-12">
+    <div class="p-12 flex justify-center text-2xl ">
+      <div class="flex gap-4">
         <h1>FOLLOW US</h1>
         <div>Facebook</div>
         <div>Twitter</div>
