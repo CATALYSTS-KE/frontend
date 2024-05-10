d24bcd24c644f81fde6cec7681326a59984b8b01
nashthecoder <naijeria@gmail.com>
2024-05-10 18:27:51 +0300

    fixed footer grid

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="footer text-white flex flex-col items-center p-10 bg-[url('@/assets/images/footer_bg.svg')]">
+  <div class="footer bg-[url('@/assets/images/footer_bg.svg')]  text-white flex flex-col items-center w-full h-full">
     <!-- link to the pages container -->
-    <div class="flex px-4">
-      <div>
+    <div class="grid md:grid-cols-2 lg:grid-cols-4">
+      <div class="mb-6 mx-20">
         <h1>ABOUT US</h1>
         <div class="flex flex-col">
-          <RouterLink to="/">Who we are</RouterLink>
+          <RouterLink to="/">Who We Are</RouterLink>
           <RouterLink to="/"> What We Do</RouterLink>
         </div>
       </div>
-      <div class="">
+      <div class="mb-6">
         <h1>UPDATES</h1>
-        <div class="flex flex-col px-4">
+        <div class="flex flex-col ">
           <RouterLink to="/">Stories</RouterLink>
           <RouterLink to="/">News/Articles</RouterLink>
           <RouterLink to="/">Events</RouterLink>
         </div>
       </div>
-      <div>
+      <div class="mb-6">
         <h1>RESOURCES</h1>
-        <div class="flex flex-col px-4">
+        <div class="flex flex-col">
           <RouterLink to="/">Guidelines</RouterLink>
           <RouterLink to="/">Toolkits</RouterLink>
           <RouterLink to="/">Resources</RouterLink>
         </div>
       </div>
-      <div>
+      <div class="mb-6">
         <h1>JOIN US</h1>
-        <div class="flex flex-col px-4">
+        <div class="flex flex-col">
           <RouterLink to="/">Alliance Member</RouterLink>
           <RouterLink to="/">Donor</RouterLink>
           <RouterLink to="/">Corporate</RouterLink>
@@ -35,11 +35,12 @@
       </div>
     </div>
     <!-- contact info -->
-    <div class="p-4"><span>CONTACTS:</span><span> www.catalystsafrica.org</span></div>
+    <div class="pt-8">
+      <span class="text-yellow-600">CONTACTS:</span><span> www.catalystsafrica.org</span></div>
     <!-- socials -->
-    <div>
-      <div class="flex gap-4">
-        <p>FOLLOW US</p>
+    <div class="p-12 flex justify-center ">
+      <div class="flex gap-4 pb-12">
+        <h1>FOLLOW US</h1>
         <div>Facebook</div>
         <div>Twitter</div>
         <div>Instagram</div>
@@ -54,12 +55,13 @@
 import { RouterLink } from 'vue-router'
 </script>
 
- <style scoped>
+<style scoped>
 .footer {
+  background-size: cover;
+  background-position: center;
   background-repeat: no-repeat;
   margin-top: 0px ;
   padding-top: 100px;
-  height: 300px; /* Adjust the height as needed to display the image */
   text-align: center;
 }
 
