2f7ce2eeeb80e23365267899a450e9da8a23519c
Chanice Irungu <chaniceashley@gmail.com>
2024-05-11 00:19:44 +0300

    Join us page

---
+++ src/components/common/RectangleSvg.vue
@@ -0,0 +1,64 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path :d="path" :fill="fill" />
+    <text
+      x="50%"
+      y="50%"
+      dominant-baseline="middle"
+      text-anchor="middle"
+      :fill="textFill"
+      class="uppercase"
+      :font-size="calculateFontSize"
+    >
+      {{ text }}
+    </text>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    fill: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    textFill: {
+      type: String,
+      default: '#FFF'
+    },
+    width: {
+      type: Number,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: true
+    },
+    viewBox: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      required: true,
+      default: 'M362.841 11.544V91.974L3.64062 105.074V6.84399L362.841 11.544Z'
+    }
+  },
+  computed: {
+    calculateFontSize() {
+      // Adjust the factor (0.8) as needed to occupy almost the whole width
+      return Math.floor((this.width * 0.8) / this.text.length)
+    }
+  }
+}
+</script>
--- src/views/JoinUsView.vue
+++ src/views/JoinUsView.vue
@@ -1,40 +1,83 @@
 <template>
   <div>
     <main>
-      <section>
-        <div>Support image Header</div>
-        <!-- orginationz -->
-        <div>
-          <!-- one organization -->
-          <div>
-            <div>Image for organization</div>
-            <h1>some header</h1>
-            <p>some tagline</p>
-          </div>
+      <section
+        class="pb-10 px-20 bg-[url('@/assets/images/pattern_vector_grey.svg')] bg-cover bg-center relative overflow-hidden"
+      >
+        <div class="p-4">
+          <!-- ReusableSvg component -->
+          <RectangleSvg
+            fill="#EE3B26"
+            width="371"
+            height="116"
+            viewBox="0 0 371 116"
+            :path="path1"
+            text="Support"
+            textFill="white"
+            class=""
+          />
         </div>
 
-        <div>
-          <button>JOIN AS DONOR</button>
-          <button>JOIN AS ALLIANCE</button>
-        </div>
-      </section>
-      <section>
-        <div>
-          <div>HOW TO BECOME A MEMBER iMAGE headER</div>
-          <!-- info card -->
-          <div>
-            <!--  -->
-            <h1>lorenm ipsum join</h1>
-            <p>Lorem Ipsum pargrapgh</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <!-- Loop through organizations -->
+          <div
+            v-for="index in 8"
+            :key="index"
+            class="border-2 bg-white border-gray-400 rounded-lg p-4"
+          >
+            <div class="flex flex-col justify-center">
+              <img
+                src="@/assets/images/organization.png"
+                alt="Organization"
+                class="mb-4 rounded-md"
+              />
+              <h2 class="text-lg font-semibold mb-2">Organization Title</h2>
+              <p class="text-gray-600">Organization Tagline</p>
+            </div>
           </div>
         </div>
-      </section>
+        <div class="flex justify-between">
+          <RectangleSvg
+            fill="#00C4B3"
+            width="450"
+            height="116"
+            viewBox="0 0 614 116"
+            :path="path2"
+            text="Join As A Donor"
+            textFill="white"
+            class=""
+          />
 
-      <section>
-        <div>
-          <button>Downlaod Phot0 Frames</button>
+          <RectangleSvg
+            fill="#943700"
+            width="450"
+            height="116"
+            viewBox="0 0 613 116"
+            :path="path3"
+            text="Join the alliance"
+            textFill="white"
+            class=""
+          />
         </div>
+      </section>
+      <section class="bg-[#808184] flex justify-center">
+        <RectangleSvg
+          fill="#CDDE00"
+          width="450"
+          height="116"
+          viewBox="0 0 613 116"
+          :path="path3"
+          text="Download Photo Frames"
+          textFill="black"
+          class=""
+        />
       </section>
     </main>
   </div>
 </template>
+<script setup>
+import RectangleSvg from '@/components/common/RectangleSvg.vue'
+const path1 = 'M362.841 11.544V91.974L3.64062 105.074V6.84399L362.841 11.544Z'
+const path2 = 'M607.84 12.09V92.51L3.2002 105.61V7.38L607.84 12.09Z'
+const path3 = 'M3 100.9V20.48L607.64 7.38V105.61L3 100.9Z'
+</script>
