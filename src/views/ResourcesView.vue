f6be209c613a4c14a003cbff17c9d25924f57473
nashthecoder <naijeria@gmail.com>
2024-05-11 00:42:49 +0300

    added resource page top content

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,23 +1,67 @@
 <template>
   <div>
     <main>
-      <section>
-        <div>
-          <div>Research Image head</div>
-          <!-- research items -->
-          <div>
-            <!-- placeholder for one research item -->
-            <div>
-              <h1>Overview of abortion laws by county in Africa</h1>
-              <p>Lorem Ipsum pargrapgh</p>
-              <div>
-                <button>DOWNLOAD</button>
-                <button>SHARE</button>
-              </div>
-            </div>
-          </div>
+      <section class="relative h-screen px-20 overflow-hidden">
+        <div class="absolute inset-0 z-0">
+          <div class="bg-[url('@/assets/images/research_bg_top.svg')] bg-cover bg-center h-full"></div>
         </div>
+        <!-- Title image -->
+        <div class="relative z-10">
+          <img src="@/assets/images/research_title.svg" alt="Title Image" class="h-auto block">
+        </div>
+          <!-- Bottom background image -->
+        <div class="absolute inset-0 z-0">
+          <div class="bg-[url('@/assets/images/news_bg.png')] bg-cover bg-center h-full"></div>
+        </div>
+        <!-- card with white bg -->
+        <div class="w-full bg-white rounded-lg shadow-lg overflow-y-auto px-20 py-10 mt-10 mx-auto relative z-10">
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Overview of abortion laws by country in Africa</h1>
+          <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>  
+          
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 pt-16">Overview of abortion laws by country in Africa</h1>
+            <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>
+
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 pt-16">Overview of abortion laws by country in Africa</h1>
+            <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipt tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>    
+          </div>
       </section>
     </main>
   </div>
 </template>
+
