dc4a92a70d9e794f34a83bfd4a42c031be608ee6
nashthecoder <naijeria@gmail.com>
2024-05-10 13:46:01 +0300

    added text cards to news and press sections

--- src/views/UpdatesView.vue
+++ src/views/UpdatesView.vue
@@ -22,11 +22,36 @@
         <div>
         <!-- card with white bg -->
         <div class="w-full bg-white rounded-lg shadow-lg overflow-y-auto px-20 py-10 mt-10 mx-auto ">
-          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Clearly outline the pillars that guide Catalysts</h1>
-            <p class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>
-            <p class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>
-            <p class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>
-        </div>
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Overview of abortion laws by country in Africa</h1>
+          <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>  
+          
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Overview of abortion laws by country in Africa</h1>
+            <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>
+          </div>
         </div>
       </section>
       <section class="h-screen pb-10 px-20 bg-[url('@/assets/images/testimonies_bg.png')] bg-cover bg-center relative overflow-hidden">
@@ -48,33 +73,75 @@
           <img src="@/assets/images/press_title.svg" alt="Title Image" class="h-auto block relative z-10">
         </div>
         <!-- press statement one -->
-        <div>
-          <div>
-            <h1>Header One Here</h1>
-            <p>Paragraph</p>
-            <div>scroll</div>
+        <div class="w-full bg-white rounded-lg shadow-lg overflow-y-auto px-20 py-10 mt-10 mx-auto ">
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Overview of abortion laws by country in Africa</h1>
+          <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>  
           </div>
-        </div>
+          <div class="w-full bg-white rounded-lg shadow-lg overflow-y-auto px-20 py-10 mt-10 mx-auto ">
+          <h1 class="font-inter text-4xl font-normal leading-tight text-left text-red-500 px-4 py-2">Overview of abortion laws by country in Africa</h1>
+          <p Lorem class="font-inter text-lg font-normal leading-snug px-4 py-2 text-justify pb-10">
+              Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper 
+              malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices 
+              sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus 
+              vestibulum sed arcu non. Suscipit tellus mauris a diam maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. 
+              Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget. 
+              Vel pharetra vel turpis nunc eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. 
+              Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. 
+              Ac turpis egestas sed tempus. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam 
+              maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla aliquet porttitor lacus luctus 
+              accumsan tortor posuere. Eu sem integer vitae justo eget. Vel pharetra vel turpis nunc eget.</p>  
+          </div>
       </section>
 
-      <section class="h-screen pb-10 px-20 bg-[url('@/assets/images/events_bg.png')] bg-cover bg-center relative overflow-hidden">
+      <section class="h-screen px-20 bg-[url('@/assets/images/events_bg.png')] bg-cover bg-center relative overflow-hidden">
         <div>
           <img src="@/assets/images/events_title.svg" alt="Title Image" class="h-auto block relative z-10">
         </div>
         <!-- Image courousle for events 3 at a time -->
-        <div>
-          <!-- One card for event  -->
-          <div>
-            <div>
-              <h1>Image holder for card</h1>
+    
+          <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-20">
+            <!-- Event Card 1 -->
+
+
+            <!-- Event Card 2 -->
+            <div class="bg-white rounded-t-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 w-5/6">
+                <div class="rounded-lg">
+                  <img src="@/assets/images/team_pic.png" alt="Title Image" class="h-auto block relative z-10">
+                </div>
+                <p class=" text-gray-500 mb-4 font-inter text-4xl font-normal leading-snug text-left px-4">Header One here</p>
+                <p class="font-inter text-lg text-gray-500 font-normal leading-snug px-4 text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero 
+                  nunc consequat interdum.  
+                </p>  
+              <div class="p-4 flex justify-between">
+                <button
+                  class="w-2/6 bg-green-500 text-white uppercase px-4 py-2 hover:bg-green-700 focus:outline-none focus:shadow-outline-green active:bg-green-800">
+                  5th August
+                </button>
+                <button
+                  class="w-2/6 bg-green-500 text-black uppercase  px-4 py-2 hover:bg-green-700 focus:outline-none focus:shadow-outline-green active:bg-green-800">
+                  Nairobi
+                </button>
+              </div>
             </div>
-            <div>
-              <h1>header</h1>
-              <div>paragraph</div>
-              <div>date</div>
-              <div>place</div>
-            </div>
-          </div>
+
+            <!-- Event Card 3 -->
+
         </div>
       </section>
       <!-- Photo Gallery -->
