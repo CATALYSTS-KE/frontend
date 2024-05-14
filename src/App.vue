5796e0f73f51f49a6c51895ed49fea7ab4ed06d1
Chanice Irungu <chaniceashley@gmail.com>
2024-05-14 21:21:54 +0300

    rmv padding from main page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import MainFooter from '@/components/MainFooter.vue'
 </script>
 
 <template class="">
-  <div class="px-4 mx-auto min-h-screen flex flex-col relative">
+  <div class="md:px-4 mx-auto min-h-screen flex flex-col relative">
     <!-- <header class="flex items-center justify-between px-24 h-56"> -->
     <MainHeader />
     <RouterView class="wrapper relative" />
