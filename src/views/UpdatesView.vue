d02e8750dea31eef4ffe2d26ddbd44a64f119b36
nashthecoder <naijeria@gmail.com>
2024-05-15 11:52:57 +0200

    added cards to aupcoming events section on update page

--- src/views/UpdatesView.vue
+++ src/views/UpdatesView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <!-- <div>
     <img src="@/assets/images/coming_soon.jpg" />
-  </div>
+  </div> -->
   <div class="hidden">
     <!-- News & Articles  -->
     <section class="pb-10 -mt-6 bg-teal-400">
@@ -41,7 +41,7 @@
             class="text"
           />
         </div>
-        <div class="pl-40 grid grid-cols-2 2xl:grid-cols-4 gap-y-8 2xl:gap-6 pt-12">
+        <div class="md:pl-10 xl:pl-20 2xl:pl-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-12">
           <!-- Loop through frames -->
           <div
             v-for="index in 3"
@@ -50,7 +50,7 @@
           >
             <div class="px-4">
               <img src="@/assets/images/team_pic.png" alt="Organization" class="mb-4 w-full" />
-              <h1>Header One here</h1>
+              <h1>Header {{ index }} here</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
             </div>
           </div>
@@ -79,11 +79,11 @@
           <div
             v-for="(item, index) in cardData"
             :key="index"
-            class="bg-white shadow-md rounded-lg h-48 overflow-auto"
+            class="bg-white shadow-md rounded-lg h-64 overflow-auto m-8 px-4"
           >
             <div class="flex flex-col md:flex-row">
               <!-- Text Content -->
-              <div class="p-4">
+              <div class="p-12">
                 <h2 class="text-2xl px-12 text-red-primary">{{ item.title }}</h2>
                 <p
                   class="text-lg font-normal leading-snug px-12 py-6 text-justify mt-2 text-gray-600"
@@ -95,15 +95,10 @@
         </div>
       </div>
     </section>
-    <!-- Upcoming Events  -->
-    <section class="pb-10 -mt-6 bg-orange-700 bg-cover bg-center relative overflow-hidden">
-      <!-- <section
-      class="pb-10 -mt-6 bg-orange-700 bg-[url('@/assets/images/brown_bg.svg')] bg-cover bg-center relative overflow-hidden"
-    > -->
-      <!-- Container -->
+  </div>
+    <section class="pb-10 -mt-6 bg-[url('@/assets/images/events_bg.png')] bg-cover bg-center relative overflow-hidden">
       <div class="px-20">
         <div class="px-4">
-          <!-- ReusableSvg component -->
           <RectangleSvg
             fill="#00C4B3"
             width="482"
@@ -112,15 +107,56 @@
             :path="path3"
             text="Upcoming Events"
             textFill="white"
+            class="text"/>
+          </div>
+      </div>
+      <div class="pt-4 grid-cols-1 sm:grid md:grid-cols-3">
+        <div v-for="index in 3" :key="index" class=" flex flex-col items-center">
+          <img
+            src="@/assets/images/team_pic.png"
+            alt="Organization"
+            class="mb-4 rounded-md w-2/3"
+          />
+          <div class="bg-white m-2 p-8 rounded-md w-2/3">
+            <h3 class="font-semibold text-xl text-gray-400">Header one here</h3>
+            <p class="mt-1 text-gray-500 text-justify pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc consequat interdum. Eget arcu dictum varius duis at.</p>
+            <RectangleSvg
+              fill="#EE3b26"
+              width="250"
+              height="99"
+              viewBox="50 0 400 60"
+              :path="path4"
+              text="Location"
+              textFill="white"
+              class="text"
+            />
+          </div>
+          <div class="flex flex col -mt-4">
+            <RectangleSvg
+            fill="#00C4B3"
+            width="250"
+            height="99"
+            viewBox="10 0 200 50"
+            :path="path1"
+            text="Register"
+            textFill="white"
             class="text"
           />
-        </div>
-
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-20">
-          <!-- Event Card 1 -->
+          <RectangleSvg
+            fill="#F7A800"
+            width="250"
+            height="99"
+            viewBox="10 0 200 50"
+            :path="path1"
+            text="Remind Me"
+            textFill="black"
+            class="text"
+          />
+          </div>
         </div>
       </div>
     </section>
+    <div class="hidden">
     <!-- Photo Gallery  -->
     <section class="pb-10 -mt-6 bg-red-600 overflow-hidden">
       <!-- Container -->
