40adde7d3aaba33591b51b1abb7fabf15787eb87
Chanice Irungu <chaniceashley@gmail.com>
2024-06-14 12:45:21 +0300

    rmv typed

--- netlify/functions/sendmail.mjs
+++ netlify/functions/sendmail.mjs
@@ -1,7 +1,6 @@
-export const handler: Handler = async (event, context) => {
-    return {
-      body: JSON.stringify({ message: "Hello World" }),
-      statusCode: 200
-    };
-  };
-  
+export const handler = async (event, context) => {
+  return {
+    body: JSON.stringify({ message: 'Hello World' }),
+    statusCode: 200
+  }
+}
