6852720730e49f54f3de321ed8196be41a5c5d0a
Chanice Irungu <chaniceashley@gmail.com>
2024-06-19 00:12:25 +0300

    resource page edits

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="min-h-screen">
-    <section class="section-with-background min-h-screen">
+    <div class="md:pb-14 pb-10 w-full h-full pl-20">
+      <img
+        src="@/assets/images/research_title.svg"
+        alt="Research"
+        class="md:block hidden relative"
+      />
+      <img
+        src="@/assets/images/research_title_sm.svg"
+        alt="Research"
+        class="block md:hidden relative px-4 z-10"
+      />
+    </div>
+    <section class="section-with-background min-h-screen px-4">
+      <div v-for="resource in resources" :key="resource.section" class="mb-8">
+        <h2
+          class="text-2xl text-center text-white text-bold font-bold mb-4 bg-[#EE3B27] p-4 uppercase"
+        >
+          {{ resource.section }}
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pb-10">
+          <div
+            v-for="item in resource.items"
+            :key="item.url"
+            :style="{ backgroundColor: item.backgroundColor }"
+            class="p-8 rounded-md shadow-md hover:bg-opacity-90 transition duration-300"
+          >
+            <a :href="item.url" target="_blank" class="text-black hover:text-white uppercase">
+              <h3 class="text-lg font-semibold">{{ item.title }}</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <!-- <div class="w-full h-full">
         <img src="@/assets/images/coming_soon.webp" alt="Coming Soon" class="w-full h-full" />
       </div> -->
@@ -57,17 +89,136 @@
 <script setup>
 // import { jsPDF } from 'jspdf'
 
-// const cardData = {
-//   title: 'Overview of Abortion laws by country, In Africa',
-//   message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-//       incididunt ut labore et dolore magna aliqua. Ullamcorper malesuada proin libero nunc
-//       consequat interdum. Eget arcu dictum varius duis at. Sollicitudin aliquam ultrices
-//       sagittis orci a scelerisque purus semper eget. Ac turpis egestas sed tempus. Tempor
-//       commodo ullamcorper a lacus vestibulum sed arcu non. Suscipit tellus mauris a diam
-//       maecenas sed enim ut sem. Viverra nibh cras pulvinar mattis. Vulputate enim nulla
-//       aliquet porttitor lacus luctus accumsan tortor posuere. Eu sem integer vitae justo eget.
-//       Vel pharetra vel turpis nunc eget.`
-// }
+const resources = [
+  {
+    section: 'GUIDANCE DOCUMENTS and WEBSITES',
+    items: [
+      {
+        title: 'WHO Abortion Care Guidelines (2022)',
+        url: 'https://srhr.org/abortioncare/',
+        backgroundColor: '#C4D93C'
+      },
+      {
+        title: 'IPPF Comprehensive Abortion Care Guidelines',
+        url: 'https://ippfmaforum.org/2021/10/06/abortion-care-guidelines/',
+        backgroundColor: '#00C5B4'
+      },
+      {
+        title:
+          'SafeAccess - Website sharing best practice guidance, news and resources on safe abortion and post-abortion care',
+        url: 'https://www.safeaccesshub.org/about/',
+        backgroundColor: '#F4A60E'
+      }
+    ]
+  },
+  {
+    section: 'TRAINING RESOURCES',
+    items: [
+      {
+        title: 'Online medical abortion course (WHO)',
+        url: 'https://lms-staging.lxp.academy.who.int/courses/course-v1:WHOA+0012_MA_EN+2023/about',
+        backgroundColor: '#933801'
+      },
+      {
+        title: 'Mobile app for health care workers providing abortion care (WHO)',
+        url: 'https://www.who.int/news/item/07-08-2023-new-mobile-phone-app-helps-healthcare-workers-provide-comprehensive-abortion-care#:~:text=In%20recognition%20of%20this%2C%20the,the%20WHO%20Abortion%20care%20guideline%2C',
+        backgroundColor: '#443635'
+      },
+      {
+        title: 'HowtoUse online medical abortion training for providers',
+        url: 'https://elearning.howtouseabortionpill.org/',
+        backgroundColor: '#EE3B27'
+      }
+    ]
+  },
+  {
+    section: 'COMMUNICATING and EDUCATING about ABORTION',
+    items: [
+      {
+        title: 'How to Talk about Abortion: A guide to stigma-free messaging (IPPF)',
+        url: 'https://www.ippf.org/resource/how-talk-about-abortion-guide-stigma-free-messaging',
+        backgroundColor: '#8D7C34'
+      },
+      {
+        title: 'Women on Web Abortion Framing Toolkit (Women on Web)',
+        url: 'https://www.womenonweb.org/en/page/21991/abortion-framing-toolkit',
+        backgroundColor: '#F7A901'
+      },
+      {
+        title:
+          'How to Educate about abortion: A guide for peer educators, teachers and trainers (IPPF)',
+        url: 'https://www.ippf.org/resource/How-educate-about-abortion-guide-peer-educators-teachers-and-trainers',
+        backgroundColor: '#CDDD00'
+      }
+    ]
+  },
+  {
+    section: 'STIGMA and ATTITUDES',
+    items: [
+      {
+        title: 'VCAT Facilitator Guide for Global Audience (Ipas)',
+        url: 'https://www.ipas.org/resource/abortion-values-clarification-for-action-and-transformation-vcat/',
+        backgroundColor: '#933801'
+      },
+      {
+        title: 'VCAT Abortion self-care facilitator’s guide (Ipas)',
+        url: 'https://www.ipas.org/resource/abortion-self-care-values-clarification-for-action-and-transformation-workshop-facilitators-guide/',
+        backgroundColor: '#EB3D24'
+      },
+      {
+        title:
+          'Abortion attitude transformation: Values clarification activities adapted for young women (Ipas)',
+        url: 'https://www.ipas.org/resource/abortion-attitude-transformation-values-clarification-activities-adapted-for-young-women/',
+        backgroundColor: '#443737'
+      },
+      {
+        title: 'Values clarification / VCAT facilitators training (MSI)',
+        url: 'https://kayaconnect.org/course/info.php?id=5040',
+        backgroundColor: '#C4D93C'
+      },
+      {
+        title: 'Disability inclusion in reproductive health programs (Ipas)',
+        url: 'https://www.ipas.org/resource/disability-inclusion-in-reproductive-health-programs-an-orientation-and-values-clarification-toolkit/',
+        backgroundColor: '#00C5B4'
+      },
+      {
+        title:
+          'Abortion Attitude Transformation: A values clarification toolkit for humanitarian audiences (Ipas)',
+        url: 'https://www.ipas.org/resource/abortion-attitude-transformation-a-values-clarification-toolkit-for-humanitarian-audiences/',
+        backgroundColor: '#F4A60E'
+      }
+    ]
+  },
+  {
+    section: 'NEW PUBLICATIONS',
+    items: [
+      {
+        title:
+          'Progress toward abortion access and quality care: Examples from across the globe. 2024 (IJGO)',
+        url: 'https://obgyn.onlinelibrary.wiley.com/toc/18793479/2024/164/S1',
+        backgroundColor: '#8D7C34'
+      },
+      {
+        title:
+          'Domestication of the Maputo Protocol in the Democratic Republic of Congo: Leveraging regional human rights commitments for abortion decriminalization and access 2024 (IJGO)',
+        url: 'https://obgyn.onlinelibrary.wiley.com/doi/full/10.1002/ijgo.15332',
+        backgroundColor: '#CDDD00'
+      },
+      {
+        title:
+          "Professional societies of obstetrics and gynecology as agents of change in sexual and reproductive health: FIGO's 10-country safe abortion advocacy project in Africa and Latin America. 2024 (IJGO)",
+        url: 'https://obgyn.onlinelibrary.wiley.com/doi/full/10.1002/ijgo.15334',
+        backgroundColor: '#00C5B4'
+      },
+      {
+        title:
+          'Self-Managed Abortion in Africa: The Decriminalization Imperative in Regional Human Rights Standards, 2023, Health Hum Rights',
+        url: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10309143/',
+        backgroundColor: '#F4A60E'
+      }
+    ]
+  }
+]
 
 // const downloadPDF = (item) => {
 //   // Create a new jsPDF instance
@@ -102,9 +253,7 @@
 /* .section-with-background {
   @apply relative bg-[rgba(195,217,60,1)] bg-[url('@/assets/images/pattern_green.png')] bg-cover bg-center py-12;
 } */
-.section-with-background {
-  @apply relative bg-[rgba(195,217,60,1)] bg-[url('@/assets/images/coming_soon_2.webp')] bg-cover bg-center;
-}
+
 ::-webkit-scrollbar {
   width: 16px;
 }
