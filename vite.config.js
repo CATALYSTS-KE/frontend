8745ea697b1e7e37566fbbf55212fd27bb71c6b7
Chanice Irungu <chaniceashley@gmail.com>
2024-06-28 19:30:05 +0300

    generating site-map

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createSitemap } from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createSitemap({
+      hostname: 'https://catalysts-africa.netlify.app', // Your site URL
+      routes: [
+        '/',
+        '/who-we-are',
+        '/what-we-do',
+        '/updates',
+        '/resources',
+        '/join-us',
+        '/contact-us',
+        '/privacy-policy',
+        '/test'
+      ]
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
