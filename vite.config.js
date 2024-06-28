9ca2f2d8b27fc6cef6fb91b29d84d5ae2ac677d3
Chanice Irungu <chaniceashley@gmail.com>
2024-06-28 22:03:26 +0300

    ft:added manual sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Sitemap  from 'vite-plugin-sitemap'
+// import createSitemap from 'vite-plugin-sitemap'
+// import { router } from './src/router/index.js'
+// import Pages from 'vite-plugin-pages'
+// hello
+// console.log(
+//   'Routes for Sitemap:',
+//   router.routes.map((route) => route)
+// ) // Debugging line
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    Sitemap({
-      hostname: 'https://catalystsafrica.org', // Your site URL
-      routes: [
-        '/',
-        '/who-we-are',
-        '/what-we-do',
-        '/updates',
-        '/resources',
-        '/join-us',
-        '/contact-us',
-        '/privacy-policy',
-        '/test'
-      ]
-    })
+    vue()
+    // createSitemap({
+    //   hostname: 'https://catalystsafrica.org', // Your site URL
+    //   routes: [
+    //     '/',
+    //     '/who-we-are',
+    //     '/what-we-do',
+    //     '/updates',
+    //     '/resources',
+    //     '/join-us',
+    //     '/contact-us',
+    //     '/privacy-policy',
+    //     '/test'
+    //   ]
+    // })
   ],
   resolve: {
     alias: {
